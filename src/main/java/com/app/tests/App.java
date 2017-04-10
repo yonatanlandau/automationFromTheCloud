fba84c54821bdbfd5386954544580d7c57ee8b6c
Yonatan Landau <yonatanl@folloze.com>
2017-04-10 13:37:09 +0300

    New commit

--- src/main/java/com/app/tests/App.java
+++ src/main/java/com/app/tests/App.java
@@ -1,7 +1,6 @@
 package com.app.tests;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
@@ -10,17 +9,23 @@ import org.testng.annotations.Test;
 
 public class App {
 
-		WebDriver driver;
-	
-		@Test
-		public void test(){
-		System.setProperty("webdriver.chrome.driver", "C:/Users/FollozePC/projects/TestsInCloud/src/test/resources/chromedriver2.exe");
-		driver = new ChromeDriver();
-		driver.get("http://app.folloze.com");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
+	WebDriver driver;
+
+	@Test
+	public void test(){
+		try{
+
+			System.setProperty("webdriver.chrome.driver", "C:/Users/FollozePC/projects/TestsInCloud/src/test/resources/chromedriver2.exe");
+			driver = new ChromeDriver();
+			driver.get("http://app.folloze.com");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+		}catch (Exception e){
+			System.out.println("URL not found");
 		}
-		
-	
+
+	}
+
+
 
 }
