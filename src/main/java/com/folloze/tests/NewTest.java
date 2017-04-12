e68473b2fc40f326fd453f50823c05671b7f7fa7
Yonatan Landau <yonatanl@folloze.com>
2017-04-12 11:55:08 +0300

    Remove log

--- src/main/java/com/folloze/tests/NewTest.java
+++ src/main/java/com/folloze/tests/NewTest.java
@@ -1,33 +1,19 @@
 package com.folloze.tests;
 
-import java.awt.AWTException;
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import com.folloze.data.ObjectMap;
-import com.folloze.pages.BoardPage;
 import com.folloze.pages.CompanyAdmin;
-import com.folloze.pages.CreateAccount;
 import com.folloze.pages.Login;
 import com.folloze.pages.Onboarding;
-import com.folloze.pages.OpenIncognito;
-import com.folloze.pages.Yopmail;
+
 
 public class NewTest {
 
 	private WebDriver driver;
-	Logger logger;
-
+	
 	@BeforeClass
 	public void beforeClass() {
 
