c3cc187ff8a8d1096dea51fa5ba83c259145996e
Yonatan Landau <yonatanl@folloze.com>
2017-04-29 09:48:05 +0300

    wip

--- src/main/java/com/folloze/utils/Utils.java
+++ src/main/java/com/folloze/utils/Utils.java
@@ -8,25 +8,23 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class Utils {
 
 	public static WebDriver getDriver(){
 		WebDriver driver;
-//		System.setProperty("webdriver.chrome.driver", "//src/main/resources/chromedriver.exe");
-//		ChromeOptions options = new ChromeOptions();
-//		options.addArguments("disable-infobars");
-//		options.addArguments("--start-maximized");
-//		options.addArguments("--disable-web-security");
-//		options.addArguments("--no-proxy-server");
-//		Map<String, Object> prefs = new HashMap<String, Object>();
-//		prefs.put("credentials_enable_service", false);
-//		prefs.put("profile.password_manager_enabled", false);
-//		options.setExperimentalOption("prefs", prefs);
-		driver = new FirefoxDriver();
-		//driver = new ChromeDriver(options);
+		System.setProperty("webdriver.chrome.driver", "//src/main/resources/chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("disable-infobars");
+		options.addArguments("--start-maximized");
+		options.addArguments("--disable-web-security");
+		options.addArguments("--no-proxy-server");
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("credentials_enable_service", false);
+		prefs.put("profile.password_manager_enabled", false);
+		options.setExperimentalOption("prefs", prefs);
+		driver = new ChromeDriver(options);
 		driver.get("http://app.folloze.com");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
